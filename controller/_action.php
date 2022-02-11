76b7159c5b0c47d9a7fff8e4fb9523702958617e
Pablo <pablo.marcano.16@gmail.com>
2022-02-11 12:08:15 -0400

    change name

--- controller/_action.php
+++ controller/_action.php
@@ -1,7 +1,7 @@
 <?php
-
+    $today = date('Y-m-d');
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://api.caribeapuesta.com/loteries/results3?since=2022-02-07&product=1'); 
+    curl_setopt($ch, CURLOPT_URL, "http://api.caribeapuesta.com/loteries/results3?since='{$today}'&product=1"); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
     curl_setopt($ch, CURLOPT_HEADER, 0); 
     $json = curl_exec($ch); 

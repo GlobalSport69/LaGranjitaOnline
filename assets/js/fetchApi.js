1c4904861c14d7c53de8e636dd8a069061ecc8ec
globalsport <globalsport69c.a@gmail.com>
2023-05-23 16:01:00 -0400

    change endpoint

--- assets/js/fetchApi.js
+++ assets/js/fetchApi.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
     myModal.toggle();
 });
 
-let baseUrl = 'https://ep443.premierpluss.com/loteries/';
+let baseUrl = 'https://webservice.premierpluss.com/loteries/';
 //Funcion redirect url prod
 function changeLink(url){
     window.open(url, '_blank');
